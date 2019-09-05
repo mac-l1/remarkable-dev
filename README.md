b7bfed9f9204acb68133ae093bc2821e0fa5f99e
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2019-09-05 21:30:00 +0200

    documenting

--- README.md
+++ README.md
@@ -1,25 +1,52 @@
 # remarkable-dev
 
+This project intends to create a proper development environment for reMarkable
+
 ## resources
 
 ### official
 
-https://remarkable.engineering/deploy/sdk/
-
-https://remarkablewiki.com/tech/filesystem
+- remarkable SDK: https://remarkable.engineering/deploy/sdk/
+- filesystem description: https://remarkablewiki.com/tech/filesystem
+- eink driver: https://github.com/reMarkable/linux/blob/137ec479df0252a6483ee4d90912dcda66330b8a/arch/arm/mach-pxa/am200epd.c
 
 ### community
 
-https://github.com/torwag/remarkableflash
+- https://github.com/torwag/remarkableflash
+- https://plasma.ninja/blog/devices/remarkable/2017/12/18/reMarkable-exporation.html
+- https://github.com/ryzenlover/remarkable-mfgtools
 
-https://plasma.ninja/blog/devices/remarkable/2017/12/18/reMarkable-exporation.html
-
-https://github.com/ryzenlover/remarkable-mfgtools
-
----
-
-http://www.davisr.me/projects/remarkable-microsd/
+- http://www.davisr.me/projects/remarkable-microsd/
 
 ### tools
 
-https://github.com/dockcross/dockcross
+- https://github.com/dockcross/dockcross
+
+### documentation
+
+#### emulating raspberry
+
+- https://www.supinfo.com/articles/single/5429-emuler-une-raspberry-pi-linux-avec-qemu
+- http://www.jdhp.org/docs/tutoriel_rpi_qemu/tutoriel_rpi_qemu.html
+- https://azeria-labs.com/emulate-raspberry-pi-with-qemu/
+- https://raspberrypi.stackexchange.com/questions/165/emulation-on-a-linux-pc
+- https://github.com/wimvanderbauwhede/limited-systems/wiki/Raspbian-"stretch"-for-Raspberry-Pi-3-on-QEMU
+
+#### creating Qemu machines
+
+- https://connect.ed-diamond.com/GNU-Linux-Magazine/GLMF-148/Qemu-comment-emuler-une-nouvelle-machine-Cas-de-l-APF27
+
+## research
+
+- download mfgtools
+- create a new image
+  - `dd if=/dev/zero of=floppy.img bs=1k count=1000000`
+  - `sudo losetup -fP floppy.img`
+  - `mkfs.ext4 floppy.img`
+- populate image
+  - `mkdir mnt`
+  - `sudo mount floppy.img ./mnt`
+  - decompress mfgtools' rootfs
+  - `cp -a` contents to the image
+- install qemu and arm dependencies
+- `./start.sh`
---
+++ start.sh
@@ -0,0 +1,58 @@
+#   -display gtk \
+#   -m 512M \
+
+qemu-system-arm \
+  -M sabrelite \
+  -bios "files/u-boot.imx" \
+  -kernel "zImage" \
+  -append "console=ttymxc0 rootfstype=ext4 root=/dev/mmcblk1p2 rw rootwait init=/bin/bash loglevel=8 bootmem-debug earlyprintk" \
+  -dtb "zero-gravitas.dtb" \
+  -drive file="floppy.img",format=raw,id=mmcblk1p2 -device sd-card,drive=mmcblk1p2
+
+# [    0.713093] 2020000.serial: ttymxc0 at MMIO 0x2020000 (irq = 19, base_baud = 5000000) is a IMX
+# [    0.732268] console [ttymxc0] enabled
+# [    0.736333] phy index low: 1, phy index high: 2
+# [  240.289647] INFO: task swapper:1 blocked for more than 120 seconds.
+# [  240.290160]       Not tainted 4.1.28-zero-gravitas-01866-ge0b823726ea4-dirty #82
+# [  240.290318] "echo 0 > /proc/sys/kernel/hung_task_timeout_secs" disables this message.
+# [  240.290662] swapper         D 8051c44c     0     1      0 0x00000000
+# [  240.292245] [<8051c44c>] (__schedule) from [<8051c73c>] (schedule+0x40/0x98)
+# [  240.292473] [<8051c73c>] (schedule) from [<8051e7b8>] (schedule_timeout+0x114/0x168)
+# [  240.292781] [<8051e7b8>] (schedule_timeout) from [<8051d248>] (wait_for_common+0x88/0x130)
+# [  240.292953] [<8051d248>] (wait_for_common) from [<80262c74>] (imx_rng_init+0x158/0x2a8)
+# [  240.293117] [<80262c74>] (imx_rng_init) from [<80262574>] (set_current_rng+0xc0/0x15c)
+# [  240.293276] [<80262574>] (set_current_rng) from [<80262874>] (hwrng_register+0x190/0x1b8)
+# [  240.293436] [<80262874>] (hwrng_register) from [<807c3fd8>] (imx_rng_probe+0xd4/0x134)
+# [  240.293682] [<807c3fd8>] (imx_rng_probe) from [<802748e0>] (platform_drv_probe+0x44/0xac)
+# [  240.293852] [<802748e0>] (platform_drv_probe) from [<802735ac>] (driver_probe_device+0x178/0x2b8)
+# [  240.294009] [<802735ac>] (driver_probe_device) from [<802737bc>] (__driver_attach+0x8c/0x90)
+# [  240.294158] [<802737bc>] (__driver_attach) from [<80271d50>] (bus_for_each_dev+0x68/0x9c)
+# [  240.294352] [<80271d50>] (bus_for_each_dev) from [<802726bc>] (bus_add_driver+0x13c/0x1e4)
+# [  240.294600] [<802726bc>] (bus_add_driver) from [<80273ed4>] (driver_register+0x78/0xf8)
+# [  240.294843] [<80273ed4>] (driver_register) from [<807c434c>] (__platform_driver_probe+0x20/0x70)
+# [  240.295092] [<807c434c>] (__platform_driver_probe) from [<807a9d78>] (do_one_initcall+0x118/0x1c4)
+# [  240.295367] [<807a9d78>] (do_one_initcall) from [<807a9f48>] (kernel_init_freeable+0x124/0x1c4)
+# [  240.295609] [<807a9f48>] (kernel_init_freeable) from [<8051883c>] (kernel_init+0x8/0xe8)
+# [  240.295844] [<8051883c>] (kernel_init) from [<8000ef88>] (ret_from_fork+0x14/0x2c)
+
+# reMarkable: ~/ cat /proc/cpuinfo
+# processor    : 0
+# model name    : ARMv7 Processor rev 10 (v7l)
+# BogoMIPS    : 48.00
+# Features    : half thumb fastmult vfp edsp neon vfpv3 tls vfpd32 
+# CPU implementer    : 0x41
+# CPU architecture: 7
+# CPU variant    : 0x2
+# CPU part    : 0xc09
+# CPU revision    : 10
+# Hardware    : Freescale i.MX6 SoloLite (Device Tree)
+# Revision    : 0000
+# Serial        : 123721d4ea9ce679
+
+# remarkable-mfgtools/Profiles/MX6SL Linux Update/OS Firmware/
+
+# qemu-system-arm \
+#   -drive file=yourfile.img,format=raw,id=mycard
+#   -device sd-card,drive=mycard
+#   -append "console=ttymxc0 rootfstype=ext4 root=/dev/mmcblk0p0 rw rootwait init=/sbin/init " \
+#   -dtb imx6dl-sabresd.dtb
