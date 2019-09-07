ad85fc50614e24565abc2c07592a6470e1a1751b
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2019-09-07 02:55:39 +0200

    fix

--- prepare.sh
+++ prepare.sh
@@ -9,6 +9,7 @@ git clone https://github.com/ryzenlover/remarkable-mfgtools
 
 # build i.MX6 vanilla kernel
 cd buildroot
+unset PERL_MM_OPT
 make imx6-sabresd_qt5_defconfig
 make linux-build
 cd -
@@ -27,7 +28,7 @@ dd if=/dev/zero of=rootfs.img bs=1M count=1024
 mkfs.ext4 -F -L linuxroot rootfs.img
 mkdir mnt
 sudo mount -o loop rootfs.img ./mnt
-cp -a rootfs/* ./mnt
+sudo cp -a rootfs/* ./mnt
 sudo umount ./mnt
 cd ../../../../
 
